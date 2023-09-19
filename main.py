b032b1b8876bb17738781959b8294cbb2fe28221
Oleksii Rubel <o.rubel@khai.edu>
2023-09-19 10:03:56 +0200

    Task 2&3 finished...

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm: task 1 & task 3')
+    print_hi('PyCharm: task 1 & task 2 & task 3')
+    print("Task 2 finished")
     print("Task 3 started..")
     print("Task 3 finished")
 
